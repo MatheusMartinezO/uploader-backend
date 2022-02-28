aa9fd903ca53f3e78a488d6b37be9c73274d5bf3
MatheusDesenvolvedor <matheus234.maritnez.oliveira@gmail.com>
2022-02-28 16:10:11 -0300

    4

--- src/index.js
+++ src/index.js
@@ -1,32 +1,29 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
-const path = require("path");
-const cors = require("cors");
-
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
 const app = express();
 
 /**
  * Database setup
  */
-mongoose.connect(
-  process.env.MONGO_URL,
-  {
-    useNewUrlParser: true
-  }
-);
+ const { MongoClient, ServerApiVersion } = require('mongodb');
+ const uri = "mongodb+srv://deploy:<123>@cluster0.f03va.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+ client.connect(err => {
+   const collection = client.db("test").collection("devices");
+   // perform actions on the collection object
+   client.close();
+ });
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(morgan("dev"));
-app.use(
-  "/files",
-  express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
-);
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev'));
+app.use('/files', express.static(path.resolve(__dirname,'..','tmp','uploads')))
+app.use(require('./routes'));
 
-app.use(require("./routes"));
-
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 3000);
