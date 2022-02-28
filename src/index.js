e6df72c971085745c19447d471d800fcd3466a67
MatheusDesenvolvedor <matheus234.maritnez.oliveira@gmail.com>
2022-02-28 15:31:42 -0300

    configurando a porta do heroku

--- src/index.js
+++ src/index.js
@@ -25,7 +25,4 @@ app.use(morgan('dev'));
 app.use('/files', express.static(path.resolve(__dirname,'..','tmp','uploads')))
 app.use(require('./routes'));
 
-app.listen(3001 , () => {
-    let data = new Date();
-    console.log('Servidor node iniciado em: ' + data);
-});
+app.listen(process.env.PORT || 3000);
