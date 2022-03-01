bcaa4ce959709c6118263f612010ba548fad699e
MatheusDesenvolvedor <matheus234.maritnez.oliveira@gmail.com>
2022-03-01 15:39:53 -0300

    outro teste

--- src/index.js
+++ src/index.js
@@ -22,6 +22,8 @@ mongoose.connect(
 app.use((req,res,next) => {
   res.header("Access-Control-Allow-Origin","*")
   res.header('Access-Control-Allow-Methods','*')
+  res.header('Access-Control-Allow-Headers','*')
+  res.header('Access-Control-Max-Age','86400')
   app.use(cors());
   next();
 })
